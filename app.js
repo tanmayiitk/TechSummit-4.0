d2059379316e9d399c268c27485bc8427d0af9ee
tanmayiitk <tanmayy.iitk@gmail.com>
2019-01-14 00:14:54 +0530

    Mobile Compatible

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ particlesJS('particles-js',
         "value": 80,
         "density": {
           "enable": true,
-          "value_area": 700
+          "value_area": 800
         }
       },
       "color": {
